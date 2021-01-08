4d4413448e0753bb50033698293aebd004ff675e
GGgun2013 <gggun2013@gmail.com>
2021-01-08 16:26:43 +0700

    lab9_3Clear

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    float Mean ,Standard,sum,sumS1,sumS2,count;
+    string text;
+    ifstream source("score.txt");
+    while(getline(source,text))
+    {
+        sum += atof(text.c_str());
+        sumS2 += pow(atof(text.c_str()),2);
+        count++;
+    }
+    Mean = (1/count)*sum;
+    sumS1 = ((1/count)*sumS2)-pow(Mean,2);
+    Standard = sqrt(sumS1);
+    cout << "Number of data = "<<count;
+    cout <<setprecision(3);
+    cout << "\nMean = "<<Mean;
+    cout << "\nStandard deviation = "<<Standard;
+    return 0;
+}
